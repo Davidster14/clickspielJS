598f9070a2d92574de1a8d445a20321da24d5c0a
meinName <Testemail@gmx.de>
2024-01-12 14:42:30 +0100

    nächster schritt

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,28 @@
+const game = new Game(30);
+
 class Game {
+
+    @param number
+
     constructor() {
         this.duration; // Dauer in sekunden
+
         this.points; // Punkte erreicht
         this.countdown; // Zeit Intervall
+
         this.status;
     }
-    start = () => {};
+    start = () => {
+        this.countdown.value = this.duration;
+        const position = getRandomPosition(target, playarea);
+        setPosition
+
+    };
     pause = () => {};
     resume = () => {};
-    stop = () => {};
+    stop = () => {
+        window 
+    };
 }
 const getRandomPostion = (playarea, target) => {
     const top = Math.random() * (playarea.height - target.height);
@@ -19,28 +33,32 @@ document.addEventListener("DOMContentLoaded"),
         console.log("Hello World");
     };
 
-const e_target = document.getElementById("target");
-const e_playarea = document.getElementById("playarea");
+const target = document.getElementById("target");
+const playarea = document.getElementById("playarea");
+
+this.countdown = window.setInterval(()
+
+
 
 const playarea = {
     width: 800,
-    height: 600
+    height: 600,
 };
 
 e_playarea.addEventListener("click"),
     (event) => {
-        console.log(event, target === e_target ? "Treffer" : "Daneben");
+        console.log(event, target === target ? "Treffer" : "Daneben");
         const top = Math.random() * (playarea.height - target.height);
         const left = Math.random() * (playarea.width - target.width);
-        e_target.style.top = "$(top)px;";
-        e_target.style.left = "$(left)px";
+        target.style.top = "$(top)px;";
+        target.style.left = "$(left)px";
     };
 
 let points = 0;
 
 const position = getRandomPostion(target, playarea);
-setPosition(position, e_target);
+Position(position, target);
 
-if (event.target === e_target) {
+if (event.target === target) {
     points += 1;
 }
